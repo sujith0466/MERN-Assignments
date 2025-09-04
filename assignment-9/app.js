6830ab4c7815a420c86889ad12444492b5369f52
Sujith Kumar Sanisetty <sujithsunil4818@gmail.com>
2025-09-04 15:37:01 +0530

    Update app.js

--- assignment-9/app.js
+++ assignment-9/app.js
@@ -7,30 +7,24 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
 const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/secretsDB";
 mongoose.connect(mongoURI);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  
 });
-
 
 const secret="thisislittlesecret.";
 userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
-
 const User = mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
   res.render("home");
 });
-
 app.get("/login", function(req, res){
   res.render("login");
 });
-
 app.get("/register", function(req, res){
   res.render("register");
 });
@@ -40,34 +34,30 @@ app.post("/register", function(req, res) {
     email: req.body.email,
     password: req.body.password
   });
-
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
+  newUser.save()
+    .then(() => {
       res.render("secrets");
-    }
-  });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 app.post("/login", function(req, res) {
   const username = req.body.email;
   const password = req.body.password;
-
-  User.findOne({ email: username }, function(err, foundUser) {
-    if (err) {
-      console.log(err);
-    } 
-    else {
+  User.findOne({ email: username })
+    .then(foundUser => {
       if (foundUser) {
         if (foundUser.password === password) {
           res.render("secrets");
-        } 
-      } 
-    }
-  });
+        }
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
-
 
 app.listen(8000, function() {
   console.log("server started on port 8000");
